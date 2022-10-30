0f5aee619dacdd61a9886b2545a405090ded5511
matthew dacanay <matthewdacanay@aol.com>
2022-10-29 18:11:16 -0700

    v1

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pygame
-pygame.init()
+import pygame, sys
 
-#colors 
+#colors
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
@@ -11,89 +10,75 @@ purple = (127, 0, 255)
 orange = (255, 165, 0) 
 gray = (192,192,192)
 
-screen = pygame.display.set_mode([1440, 800]) # setting screen size and caption
-pygame.display.set_caption('csc clicker') 
+class Player(pygame.sprite.Sprite): #animation class 
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('frame_0.png'))
+        self.sprites.append(pygame.image.load('frame_1.png'))
+        self.sprites.append(pygame.image.load('frame_2.png'))
+        self.sprites.append(pygame.image.load('frame_3.png'))
+        self.sprites.append(pygame.image.load('frame_4.png'))
+        self.sprites.append(pygame.image.load('frame_5.png'))
+        self.sprites.append(pygame.image.load('frame_6.png'))
+        self.sprites.append(pygame.image.load('frame_7.png'))
+        self.sprites.append(pygame.image.load('frame_8.png'))
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
 
-background = white 
-framerate = 60 
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [pos_x,pos_y]
+
+    def animate(self):
+        self.is_animating = True 
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += .5 # adjust this number for animation speed 
+            if self.current_sprite >=len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+            self.image = self.sprites[int(self.current_sprite)]
+
+
+pygame.init()
+clock = pygame.time.Clock()
+screen_width = 1440
+screen_height = 1000
+screen = pygame.display.set_mode((screen_width, screen_height))
 font = pygame.font.Font('freesansbold.ttf', 16)
-timer = pygame.time.Clock()
+pygame.display.set_caption("PUSH ME!!!")
 
-#game variables
-green_value = 1 
-red_value = 2 
-gray_value = 3
-purple_value = 4
-blue_value = 5
 
-draw_green = False
-draw_red = False
-draw_gray = False
-draw_purple = False 
-draw_blue = False 
+moving_sprites = pygame.sprite.Group()
+player = Player(450,150) # position of player 
+moving_sprites.add(player)
 
-length_green = 0
-length_red = 0 
-length_gray = 0 
-length_purple = 0 
-length_blue = 0
+# score 
+score_value = 0 
+font = pygame.font.Font('freesansbold.ttf',32)
+scoreX = 10 
+scoreY = 10
 
-speed_green = 5
-speed_red = 4
-speed_gray = 3
-speed_purple = 2
-speed_blue = 1
+def show_score(x,y):
+    score = font.render("Pumpkins: " + str(score_value), True, white)
+    screen.blit(score, (x,y))
 
-score = 0
-def draw_task(color, y_coord, value, draw, length, speed):
-    global score
-    if draw and length < 200:
-        length += speed 
-    elif length >= 200:
-        draw = False 
-        length = 0
-        score += value 
-
-    task = pygame.draw.circle(screen, color, (30, y_coord), 20, 5)  #constructing the ui
-    pygame.draw.rect(screen, color, [70, y_coord - 15, 200, 30])
-    pygame.draw.rect(screen, white, [75, y_coord - 10, 190,20])
-
-    pygame.draw.rect(screen, color, [70, y_coord-15, length, 30])
-
-    value_text = font.render(str(value), True, black)
-    screen.blit(value_text, [25, y_coord - 10])
-    return task, length, draw 
-
-def draw_score(color, y_coord, score, draw, length, speed):
-    pygame.draw.rect(screen, color, [70, y_coord - 15, 200, 30])
-
-running = True 
-while running: 
-    timer.tick(framerate)
+while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: 
-            running = False 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if task1.collidepoint(event.pos):
-                draw_green = True 
-            if task2.collidepoint(event.pos):
-                draw_red = True 
-            if task3.collidepoint(event.pos):
-                draw_gray = True 
-            if task4.collidepoint(event.pos):
-                draw_purple = True 
-            if task5.collidepoint(event.pos):
-                draw_blue = True
-    
-    screen.fill(background)
-    task1, length_green, draw_green = draw_task(green, 50, green_value, draw_green, length_green, speed_green)
-    task2, length_red, draw_red = draw_task(red, 100, red_value, draw_red, length_red, speed_red)
-    task3, length_gray, draw_gray = draw_task(gray, 150, gray_value, draw_gray, length_gray,speed_gray)
-    task4, length_purple, draw_purple = draw_task(purple, 200, purple_value, draw_purple, length_purple, speed_purple)
-    task5, length_blue, draw_blue = draw_task(blue, 250, blue_value, draw_blue, length_blue, speed_blue)
-
-    draw_score(gray, 150, gray_value, draw_gray, length_gray,speed_gray)
-
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            score_value += 1
+            player.animate()
+            
+    screen.fill((black))    
+    show_score(scoreX,scoreY)
+    moving_sprites.draw(screen)
+    moving_sprites.update()
     pygame.display.flip()
- 
-pygame.quit()
+    clock.tick(60)
+   
